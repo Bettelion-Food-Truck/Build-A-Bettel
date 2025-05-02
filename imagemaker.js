8ea3ab039ddad2da72bd3697f89801dbb30ec3fa
Matt <glitchtechscience@gmail.com>
2025-05-02 15:59:06 -0500

    Update imagemaker.js

--- imagemaker.js
+++ imagemaker.js
@@ -147,14 +147,17 @@ window.addEventListener('load', function (ev) {
 	 * Fetch parts info from data.json and initialize the parts variable.
 	 */
 	async function initData() {
-		fetch(ASSET_PATH + "data-compiled.json", { cache: "no-cache" }
-		).then((res) => {
+
+		/*
+		// Disabled for now; might implement later.
+		fetch(ASSET_PATH + "data-compiled.json", { cache: "no-cache" }).then((res) => {
 			if (res.ok) {
 				console.log("compiled data.json found");
 			} else {
 				console.log("compiled data.json missing");
 			}
 		});
+		*/
 
 		const response = await fetch(ASSET_PATH + "data.json", { cache: "no-cache" });
 		const json = await response.json();

c0954ea728092fe73d911240dda468af2544a872
Matt <glitchtechscience@gmail.com>
2025-05-07 02:03:04 -0500

    Moving icons and setting up assets

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,24 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+
+import { CanvasComponent } from "./components/canvas/canvas.component";
+
+import { PartsComponent } from "./components/parts/parts.component";
+import { ItemsComponent } from "./components/items/items.component";
+
+import { MovementComponent } from './components/movement/movement.component';
+import { PaletteComponent } from "./components/palette/palette.component";
+import { OutfitsComponent } from "./components/outfits/outfits.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [
+    CanvasComponent,
+    PartsComponent,
+    ItemsComponent,
+    MovementComponent,
+    PaletteComponent,
+    OutfitsComponent
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })

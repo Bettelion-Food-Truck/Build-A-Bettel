4a9d65d1e5bef5c77328ecf5b26293eac7229621
Matt <glitchtechscience@gmail.com>
2025-05-11 00:26:40 -0500

    Functional
    Ugly but works.

--- src/app/components/prompt/prompt.component.ts
+++ src/app/components/prompt/prompt.component.ts
@@ -1,15 +1,22 @@
 import { Component, Signal } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
 
 import { PromptService } from '@services/prompt/prompt.service';
 
 @Component({
   selector: 'app-prompt',
   imports: [
-    CommonModule
+    CommonModule,
+    MatButtonModule,
+    MatIconModule
   ],
   templateUrl: './prompt.component.html',
-  styleUrl: './prompt.component.scss'
+  styleUrl: './prompt.component.scss',
+  host: {
+    '[class.show]': "lastPrompt().length > 0"
+  }
 })
 export class PromptComponent {
 
@@ -20,5 +27,10 @@ export class PromptComponent {
   ) {
 
     this.lastPrompt = this.promptService.getLastPrompt();
+  }
+
+  clearPrompt() {
+
+    this.promptService.wipePrompt();
   }
 }

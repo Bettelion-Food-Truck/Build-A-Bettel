a82ae9926a0561427c7a5fd177b1af83a48f5d11
Matt <glitchtechscience@gmail.com>
2025-05-14 13:02:18 -0500

    Sort contrib pipe for weight then name sorting

---
+++ src/app/pipes/sort-contrib/sort-contributors.pipe.spec.ts
@@ -0,0 +1,8 @@
+import { SortContributorsPipe } from './sort-contributors.pipe';
+
+describe('SortContributorsPipe', () => {
+  it('create an instance', () => {
+    const pipe = new SortContributorsPipe();
+    expect(pipe).toBeTruthy();
+  });
+});

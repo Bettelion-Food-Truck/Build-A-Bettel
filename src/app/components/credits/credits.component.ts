1a7c5ad87bd5960f4092dffbcedf8286700754b0
Matt <glitchtechscience@gmail.com>
2025-05-14 13:29:10 -0500

    Rough copy to make credits component

---
+++ src/app/components/credits/credits.component.ts
@@ -0,0 +1,88 @@
+import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import {
+  MatDialogActions,
+  MatDialogClose,
+  MatDialogContent,
+  MatDialogRef,
+  MatDialogTitle,
+} from '@angular/material/dialog';
+import { MatButton } from '@angular/material/button';
+
+import { SortContributorsPipe } from 'app/pipes/sort-contrib/sort-contributors.pipe';
+
+import { CONTRIBUTOR_PATH } from '@data/paths';
+
+import data from '@data/credits.json';
+import { Group, Contributor } from '@models/credits.model';
+
+import { LogService } from '../../services/log/log.service';
+
+@Component({
+  selector: 'app-info',
+  imports: [
+    MatButton,
+    MatDialogActions,
+    MatDialogClose,
+    MatDialogTitle,
+    MatDialogContent,
+    CommonModule,
+    SortContributorsPipe
+  ],
+  changeDetection: ChangeDetectionStrategy.OnPush,
+  templateUrl: './credits.component.html',
+  styleUrl: './credits.component.scss'
+})
+export class CreditsComponent {
+  readonly dialogRef = inject(MatDialogRef<CreditsComponent>);
+
+  contributors: Group[] = [];
+  fullList: Contributor[] = [];
+
+  constructor(private logger: LogService) { }
+
+  ngOnInit() {
+
+    this.logger.info('CreditsComponent: ngOnInit()');
+
+    this.processCredits(data);
+  }
+
+  /**
+   * Process the JSON data.
+   */
+  processCredits(data: any) {
+    this.logger.info('CreditsComponent: processCredits()');
+
+    this.contributors = data.roles.map((role: any) => {
+
+      let group: Group = {
+        name: role.name ?? "",
+        section: role.section ?? "",
+        contributors: []
+      } as Group;
+
+      if (group.section.length > 0 && data[group.section]) {
+        group.contributors = data[group.section].map((contributor: any) => {
+          return {
+            name: contributor.name ?? "",
+            image: contributor.image ? `${CONTRIBUTOR_PATH}${contributor.image}` : "",
+            social: contributor.social ?? "",
+            credits: contributor.credits ?? [],
+            weight: contributor.weight ?? 0
+          } as Contributor;
+        });
+      }
+
+      return group;
+    });
+
+    this.fullList = [];
+
+    this.contributors.forEach((group: Group) => {
+
+      this.fullList.push(...group.contributors);
+    });
+  }
+}

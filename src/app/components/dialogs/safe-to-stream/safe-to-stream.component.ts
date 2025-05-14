0e58857dbe42cde916cc1db589f172c77444dfd7
Matt <glitchtechscience@gmail.com>
2025-05-14 18:51:26 -0500

    Intro lay out

--- src/app/components/dialogs/safe-to-stream/safe-to-stream.component.ts
+++ src/app/components/dialogs/safe-to-stream/safe-to-stream.component.ts
@@ -1,11 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+
+import {
+  MatDialogActions,
+  MatDialogClose,
+  MatDialogContent,
+  MatDialogRef,
+  MatDialogTitle,
+} from '@angular/material/dialog';
+import { MatCardModule } from '@angular/material/card';
+import { MatButton } from '@angular/material/button';
+
+import { DialogType } from '@components/dialogs/dialogs.enum';
 
 @Component({
   selector: 'app-safe-to-stream',
-  imports: [],
+  imports: [
+    MatButton,
+    MatDialogActions,
+    MatDialogClose,
+    MatDialogTitle,
+    MatDialogContent,
+    MatCardModule
+  ],
   templateUrl: './safe-to-stream.component.html',
   styleUrl: './safe-to-stream.component.scss'
 })
 export class SafeToStreamComponent {
+  readonly dialogRef = inject(MatDialogRef<SafeToStreamComponent>);
 
+  readonly types = DialogType;
 }
